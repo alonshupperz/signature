bc7621082c5bbe3ee755ec42bc665ecb91f8ab7e
Alon Binman <alon@Alons-Shupperz.local>
2018-10-08 11:12:02 +0300

    added new file

---
+++ signature.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="signature-output.php" method="post">
+Full Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Phone: <input type="text" name="phone"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
